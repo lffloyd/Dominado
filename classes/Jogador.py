283269967c91523c74eeb5ac59c37d2afa9aa784
vitorhardoim <vitorhardoim@id.uff.br>
2018-09-07 21:10:44 -0300

    Update Jogador.py

--- classes/Jogador.py
+++ classes/Jogador.py
@@ -1,3 +1,5 @@
+from classes.Color import *
+
 class Jogador:
     def __init__(self, ind=None):
         self.__ind = ind
@@ -22,3 +24,35 @@ class Jogador:
     def setaVez(self, seuTurno): self.vezAtual = seuTurno
 
     def pegaIndice(self): return self.__ind
+
+
+# TESTEEEE
+    def pecasJogaveis(self, mesa, mao):
+        resp = ""
+        if len(mesa.pegaTabuleiro()) == 0:
+            for peca in mao:
+                maiorPeca, nada = mesa.procuraMaiorPeca(self)
+                if peca == maiorPeca:
+                    aux = mao.index(peca)
+            for peca in mao:
+                if mao.index(peca) == aux:
+                    resp += " " + color.BLUE + color.UNDERLINE + str(mao.index(peca)+1) + color.END
+                else:
+                    resp += " " + str(mao.index(peca)+1)
+        else:
+            return
+            #for peca in mao:
+            #    if (peca.nEsq == mesa.extremoEsq() || peca.nEsq == mesa.extremoDir() || peca.nDir == mesa.extremoEsq() || peca.nDir == mesa.extremoDir()):
+            #            resp += color.BLUE + " " + mao.index(peca) + color.END
+            #    else:
+            #        resp += " " + mao.index(peca)
+        return resp
+
+    def jogar(self, mesa):
+        if self.vezAtual == False: return
+        else:
+            print("\nJogador "+str(self.__ind),self)
+            print(self.pecasJogaveis(mesa, self.mao))
+            #escolhida = input("Qual peça deseja jogar?")
+            #addMesa(mesa, self.mao[escolhida-1])
+            #del self.mao[escolhida-1]

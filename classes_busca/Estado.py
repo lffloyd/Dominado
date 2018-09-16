5894c542d57439e1b30c5b60a09aaa6fe3254c86
lffloyd <lfmatosmelo@id.uff.br>
2018-09-16 01:14:41 -0300

    'gerarEstados' method implemented in 'Estado' class. Comments added to 'Estado' and 'Expectiminimax'.

--- classes_busca/Estado.py
+++ classes_busca/Estado.py
@@ -1,8 +1,12 @@
+#Define a classe Estado, responsável por descrever o estado global de uma partida num dado momento. Usada para explorar
+#as possibilidades de jogada com os algoritmos de busca Expectiminimax e Monte-Carlo tree search.
+
+#Escrito por: Luiz Felipe, Vítor Costa, Renato Bastos
+
 import copy
 
 class Estado():
-    # Se esse estado é uma vitória, o valor de utilidade é True
-    # A mesa fornece a informação de que peças ainda estão disponiveis para a compra
+    # Define o construtor de classe, que define seus atributos internos de acordo com o momento de jogo.
     def __init__(self, jogador, oponente, mesa, turno=0):
         self.jogador = copy.deepcopy(jogador)
         self.oponente = copy.deepcopy(oponente)
@@ -10,45 +14,32 @@ class Estado():
         self.__qtdPecasComprar = len(self.mesa.pegaPecasAComprar())
         self.__qtdPecasOponente = len(self.oponente.pecas())
         self.__extremoEsq, self.__extremoDir = self.mesa.extremos()
-        self.__acoes = copy.deepcopy(jogador.pegaPecasJogaveis())
+        self.__acoes = self.gerarEstados(mesa, (jogador.possibilidadesJogaveis() if (turno == 0)
+                                                else (oponente.possibilidadesJogaveis()+
+                                                      self.mesa.pegaPecasAComprar())), 0)
         self.utilidade = 0
         #Indica qual é o jogador que executa uma jogada neste estado.
         self.turno = turno
 
-    def gerarEstados(self, mesa, mao, possibilidades, turno):
+    #Gera todas as possibilidades de estados/ações dado um nó/estado origem s. Gera as possibilidades de jogadas a partir dessa
+    #raizes. As possibilidades de ações/jogadas diferem caso turno == 0 (serão jogadas do jogador max) e caso turno == 1 (serão
+    #as possíveis jogadas do jogador min).
+    def gerarEstados(self, mesa, possibilidades):
         estadosFilhos = []
         for tupla in possibilidades:
-            peca = tupla[0]
-            pos = tupla[1]
+            peca = copy.deepcopy(tupla[0])
+            pos = copy.deepcopy(tupla[1])
             novaMesa = copy.deepcopy(mesa)
             novaMesa.adicionaNaMesa(peca, pos)
             estadosFilhos.append(novaMesa)
+        return estadosFilhos
 
-        # estados = []
-        # jogadasPossiveis = jogadasPossiveis(mesa, mao) // retornar as peças
-        # que
-        # sao
-        # possiveis
-        # de
-        # ser
-        # jogadas
-        # for peca in jogadasPossiveis:
-        #     novaMesa = mesa.copy
-        #     if pecaDoisLados:
-        #         novaMesa = jogar(novaMesa, peca, 2)
-        #         estado = Estado(novaMesa, mesa.qtdpecasoponente - -, mesa.qtdpecascomprar)
-        #         estados.append(estado)
-        #     novaMesa = jogar(novaMesa, peca)
-        #     estado = Estado(novaMesa, mesa.qtdpecasoponente - -, mesa.qtdpecascomprar)
-        #     estados.append(estado)
-        # if size(jogadasPossiveis) < mesa.pecasAComprar
-        #     estado = Estado(novaMesa, mesa.qtdpecasoponente, mesa.qtdpecascomprar - -)
-        #     estados.append(estado)
-        # return estados
-
+    #Avalia se um dado nó/estado é terminal, i.e. finaliza a partida.
     def ehEstadoTerminal(self): return len(self.__acoes) == 0
 
+    #Alterna o turno de um estado. Usado para alternar entre jogadas de max e min na exploração das possibilidades de ações.
     def alternaTurno(self): self.turno = (0 if (self.turno == 1) else 1)
 
+    #Indica se o turno atual é do oponente, i.e. se o turno atual é do jogador min.
     def turnoDoOponente(self): return self.turno == 1
 
--- classes_busca/Expectiminimax.py
+++ classes_busca/Expectiminimax.py
@@ -1,17 +1,25 @@
+#Define a classe Expectiminimax, responsável por realizar uma busca por possibilidades de ações para o jogador IA.
+
+#Escrito por: Luiz Felipe, Vítor Costa, Renato Bastos
+
 from classes_busca.Estado import *
 from classes_base.Mesa import *
 import copy
 
+#Essa classe deveria ser estática, mas isso não deve existir em Pyhton. :-|
 class Expectiminimax():
     def __init__(self):
         return
 
+    #Retorna o resultado da execução de uma dada ação (i.e. inserção de peça) num dado estado (i.e. mesa/tabuleiro). Ou
+    # seja, retorna o estado resultado da ação/jogada tomada.
     def resultado(estado, acao):
         novaMesa = copy.deepcopy(estado.mesa)
         novaMesa.adicionarNaMesa(acao.peca, acao.pos)
         return Estado(estado.jogador, estado.oponente, novaMesa)
 
-    def decisaoMinimax(self, jogador, mesa, oponente): return
+    #Inicia o procedimento de busca Expectiminax.
+    def decisaoMinimax(jogador, mesa, oponente): return
 
     def max(self): return
 

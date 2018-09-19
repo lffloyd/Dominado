d12d64b73dff017bb544977922093fc84a52ba62
lffloyd <lfmatosmelo@id.uff.br>
2018-09-19 20:45:46 -0300

    'utilidade()' method call fixed.

--- classes_busca/Estado.py
+++ classes_busca/Estado.py
@@ -20,7 +20,7 @@ class Estado():
         self.__qtdPecasOponente = len(self.oponente.pecas())
         self.acoes = []
         if (tipoEstado == self.MAX):
-            jogador.atualizaPecasJogaveis(mesa, jogador.pecas())
+            jogador.atualizaPecasJogaveis(mesa)
             self.acoes = copy.deepcopy(jogador.possibilidadesJogaveis(self.mesa))
         if (tipoEstado == self.MIN):
             self.acoes = self.possibilidadesOponente(oponente, mesa)
@@ -32,21 +32,6 @@ class Estado():
         self.tipo = tipoEstado
         #Indica o tipo de estado anterior ao atual.
         self.tipoAnterior = tipoAnterior
-
-    ###########Comentado pois pode ser útil futuramente.############
-    #Gera todas as possibilidades de estados/ações dado um nó/estado origem s. Gera as possibilidades de jogadas a partir dessa
-    #raizes. As possibilidades de ações/jogadas diferem caso turno == 0 (serão jogadas do jogador max) e caso turno == 1 (serão
-    #as possíveis jogadas do jogador min).
-    # def gerarEstados(self, mesa, possibilidades):
-    #     estadosFilhos = []
-    #     for tupla in possibilidades:
-    #         peca = copy.deepcopy(tupla[0])
-    #         pos = copy.deepcopy(tupla[1])
-    #         novaMesa = copy.deepcopy(mesa)
-    #         novaMesa.adicionaNaMesa(peca, pos)
-    #         estadosFilhos.append(novaMesa)
-    #     return estadosFilhos
-
 
     #Gera todas as possibilidades de jogada que podem ser executadas por um oponente no dado estado do jogo.
     #Considera que o oponente pode possuir todas as peças que não estão na mão do jogador ou encaixadas na mesa de jogo,
@@ -63,9 +48,9 @@ class Estado():
         esq, dir = mesa.extremos()
         for peca in pecasMesa:
             if peca.ehJogavel(esq) and peca.ehJogavel(dir):
-                possibilidades.append([peca, esq, probabilidade])
-                possibilidades.append([peca.viraPeca(), dir, probabilidade])
-            else: possibilidades.append([peca, (esq if (peca.ehJogavel(esq)) else dir), probabilidade])
+                possibilidades.append([peca, 0, probabilidade])
+                possibilidades.append([peca.viraPeca(), 1, probabilidade])
+            else: possibilidades.append([peca, (0 if (peca.ehJogavel(esq)) else 1), probabilidade])
         for jogada in possibilidades: jogada[2] = 1/(len(possibilidades)+len(oponente.possibilidadesJogaveis()))
         possibilidades = copy.deepcopy(oponente.possibilidadesJogaveis()+possibilidades)
         return possibilidades
@@ -86,4 +71,3 @@ class Estado():
     # pois o cálculo do valor de utilidade provavelmente será distinto entre eles.
     #Ainda não implementado.
     def utilidade(self): return self.valorUtilidade
-

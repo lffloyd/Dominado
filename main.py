ccec4750742d4d9440c2cf9bf039835f59234991
lffloyd <lfmatosmelo@id.uff.br>
2018-09-23 18:20:10 -0300

    'main.py' updated.

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def gameLoop(mesa, jogador1, jogador2):
         jogador1.setaGanhou(True) if (jogador1.somatorioPecas() < jogador2.somatorioPecas()) else jogador2.setaGanhou(True)
     if (jogador1.jaGanhou()): jogador1.somaPontos(jogador2.somatorioPecas())
     elif (jogador2.jaGanhou()): jogador2.somaPontos(jogador1.somatorioPecas())
-    if jogador1.jaGanhou(): print("\n\n\nJogador 1 venceu: " + str(jogador1.pegaPontos()) + "pts.")
-    else: print("\n\n\nJogador 2 venceu: " + str(jogador2.pegaPontos()) + "pts.")
+    if jogador1.jaGanhou(): print("\n\n\nJ1 venceu: " + str(jogador1.pegaPontos()) + "pts.")
+    else: print("\n\n\nJ2 venceu: " + str(jogador2.pegaPontos()) + "pts.")
 
 
 #Menu inicial do jogo.
@@ -38,20 +38,25 @@ def menu():
     if (escolha == "J") or (escolha == "j"):
         # Escolha de modos de jogo.
         print("0. Random vs. Random")
-        print("1. Humano vs. Expectiminimax")
-        print("2. Humano vs. MCTS")
-        print("3. MCTS vs. Expectiminimax")
+        print("1. Random vs. Expectiminimax")
+        print("2. Random vs. MCTS")
+        print("3. Humano vs. Expectiminimax")
+        print("4. Humano vs. MCTS")
+        print("5. MCTS vs. Expectiminimax")
         escolha = int(input())
         jogador1 = None
         jogador2 = None
         # Inicialização de objetos do jogo.
         if (escolha == 0):
             jogador1 = Jogador(1, Jogador.RANDOM)
-            jogador2 = Jogador(2, Jogador.RANDOM)
+            jogador2 = Jogador(2, Jogador.EXPECTMM)
         if (escolha > 0) and (escolha < 3):
-            jogador1 = Jogador(1, Jogador.HUMANO)
+            jogador1 = Jogador(1, Jogador.RANDOM)
             jogador2 = (Jogador(2, Jogador.EXPECTMM) if (escolha == 1) else Jogador(2, Jogador.MCTS))
-        if (escolha == 3):
+        if (escolha > 2) and (escolha < 5):
+            jogador1 = Jogador(1, Jogador.HUMANO)
+            jogador2 = (Jogador(2, Jogador.EXPECTMM) if (escolha == 3) else Jogador(2, Jogador.MCTS))
+        if (escolha == 5):
             jogador1 = Jogador(1, Jogador.MCTS)
             jogador2 = Jogador(2, Jogador.EXPECTMM)
         print("\n")

0ead938f6ccbaafc03edb5c33d78a2b414ee300c
lffloyd <lfmatosmelo@midiacom.uff.br>
2018-09-27 00:29:57 -0300

    Update EstadoMCTS.py

--- classes_busca/EstadoMCTS.py
+++ classes_busca/EstadoMCTS.py
@@ -1,3 +1,27 @@
+# MIT License
+#
+# Copyright (c) 2018 Luiz Felipe de Melo (lffloyd), Vítor Costa (vitorhardoim), Renato Bastos (RenatoBastos33)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+##############################################################################################################
+
+
 #Define a classe EstadoMCTS, responsável por descrever o estado global de uma partida num dado momento. Usada para explorar
 #as possibilidades de jogada com o algoritmo de busca 'Monte-Carlo tree search'. Observar que a classe tem diferenças em
 #relação a classe 'Estado'.
@@ -16,6 +40,7 @@ class EstadoMCTS():
         self.qtdPecasOponente = len(self.oponente.pecas())
         self.probabilidade = 0
         self.ultimaPecaJogada = None
+        self.onde=None
 
     def __str__(self):
         resp=""
@@ -29,31 +54,31 @@ class EstadoMCTS():
         for peca in self.jogador.pecas():
             achou=False
             for peca2 in estado2.jogador.pecas():
-                if peca.igual(peca2):
+                if peca==peca2:
                     achou=True
                     break
             if achou==False: return False
         for peca in self.oponente.pecas():
             igual=False
             for peca2 in estado2.oponente.pecas():
-                if peca.igual(peca2):
+                if peca==peca2:
                     igual=True
                     break
             if igual==False: return False
         onde=0
         for pecaTab in self.mesa.pegaTabuleiro():
             pecaTab2=estado2.mesa.pegaTabuleiro()[onde]
-            if not pecaTab.igual(pecaTab2): return False
+            if not pecaTab==pecaTab2: return False
             onde+=1
         return True
 
     #Indica se um estado terminal foi atingido
     def ehEstadoFinal(self):
-        print("----Teste no ehEstadoFinal()")
-        print(self)
-        print("Jogador:" + str(self.jogador.jogouRodada()))
-        print("Oponente:" + str(self.oponente.jogouRodada()))
-        print("--------Fim do teste -------\n")
+        #print("----Teste no ehEstadoFinal()")
+        #print(self)
+        #print("Jogador:" + str(self.jogador.jogouRodada()))
+        #print("Oponente:" + str(self.oponente.jogouRodada()))
+        #print("--------Fim do teste -------\n")
         if (not self.jogador.jogouRodada() and not self.oponente.jogouRodada() and len(self.mesa.pegaPecasAComprar())==0) \
                 or (len(self.jogador.pecas())==0 or len(self.oponente.pecas())==0) :
             return True
